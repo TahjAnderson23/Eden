b7c4a1125e698a60eebd3e02329cbb5d8a920a37
Tahj Anderson <tahjanderson@Tahjs-MacBook-Pro.local>
2020-08-10 21:22:33 -0400

    First push

---
+++ config.py
@@ -0,0 +1,9 @@
+import twitter
+
+def getApi():
+    return twitter.Api(consumer_key='axtXOWPhdrYlKHdX3nTitYxnS',
+                        consumer_secret='LTXnnUXJTyu5Nwee3TOR3GJ0RP28XklqELS8cRF4EbtD6mTQRC',
+                        access_token_key='1288889145939693569-jF1MiNXOyBScrDFWWEV4HwAZTgI3fI',
+                        access_token_secret='sIq5SS1Uxv9x2xKffP4xqnd0xUl9IdMvZpDJp4OAFgR4y'
+
+    )
